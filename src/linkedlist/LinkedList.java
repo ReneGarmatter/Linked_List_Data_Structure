9eab638af129401c0b19fd420254fd3970aae26e
Rene Garmatter <renegarmater@gmail.com>
2025-10-31 18:00:00 -0300

    iterator and find methods added

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,6 +1,9 @@
 package linkedlist;
+import java.lang.reflect.Array;
+import java.util.Iterator;
+import  java.util.NoSuchElementException;
 
-public class LinkedList<T> {
+public class LinkedList<T> implements Iterable<T>{
     private Node<T> head;
     private Node<T> tail;
     private int size;
@@ -154,6 +157,136 @@ public class LinkedList<T> {
                 current = current.prev;
                 index--;
             }
+        }
+    }
+
+    public T getFirst(){
+        return head.data;
+    }
+    public T getLast(){
+        return tail.data;
+    }
+
+    public int findFirstOcurrence(T object){
+        Node<T> current = head;
+        int index = 0;
+        while(current != null){
+            if(current.data == object){
+                return index;
+            }
+            else {
+                current = current.next;
+            }
+        }
+
+        return -1;
+    }
+    public int findLastOcurrence(T object){
+        Node<T> current = tail;
+        int index = size-1;
+        while(current != null){
+            if(current.data == object){
+                return index;
+            }
+            else {
+                current = current.prev;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean contains(T object){
+        Node<T> current = head;
+        while(current != null){
+            if(current.data == object){
+                return true;
+            }
+            else{
+                current = current.next;
+            }
+        }
+
+        return false;
+    }
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void clear(){
+        head = null;
+        tail = null;
+    }
+    public int size(){
+        return size;
+    }
+    @SuppressWarnings("unchecked cast")
+    public T[] toArray(T[] a){
+        if(a.length < size){
+            a = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+        }
+
+        int i=0;
+        Node<T> current = head;
+        while (current != null){
+            a[i++] = current.data;
+            current = current.next;
+        }
+
+        return a;
+    }
+
+    public void reverse(){
+        Node<T> current = head;
+        Node<T> next = current.next;
+
+        tail = current;
+        while(current != null){
+            current.next = current.prev;
+            current.prev = next;
+
+            current = next;
+            if(next != null){
+                next = next.next;
+                head = current;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListIterator();
+    }
+    private class LinkedListIterator implements Iterator<T> {
+        private Node<T> current;
+
+        public LinkedListIterator() {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T data = current.data;
+            current = current.next;
+            return data;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Remove method not supported");
         }
     }
 }
